2c225f3a7f3673a3f26ed3ff76380157e9ef0a37
Samlogy <senanisammy@gmail.com>
2021-02-23 07:29:18 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
+import { useState } from 'react';
+import { BsPlusSquare } from 'react-icons/bs'
+import { CgRemoveR } from 'react-icons/cg'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [inputList, setInputList] = useState([{ firstName: "", lastName: "" }]);
+
+    // handle input change
+    const handleInputChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...inputList];
+        list[index][name] = value;
+        setInputList(list);
+    };
+    
+    // handle click event of the Remove button
+    const handleRemoveClick = index => {
+        const list = [...inputList];
+        list.splice(index, 1);
+        setInputList(list);
+    };
+    
+    // handle click event of the Add button
+    const handleAddClick = () => {
+        setInputList([...inputList, { firstName: "", lastName: "" }]);
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='add-remove-input-field-container'>
+        {   inputList.map((x, i) => {
+                return (
+                    <div className="box">
+                        <input name="firstName" value={x.firstName} onChange={e => handleInputChange(e, i)} />
+                        
+                        <input className="ml10" name="lastName" value={x.lastName} onChange={e => handleInputChange(e, i)} />
+
+                        <div className="btn-box" >
+                            { inputList.length !== 1 &&  
+                                <CgRemoveR onClick={() => handleRemoveClick(i)} className="btn btn-error" /> }
+                            { inputList.length - 1 === i &&  
+                                <BsPlusSquare onClick={handleAddClick} className='btn btn-info' /> }
+                        </div>
+                    </div>
+                );
+            })}
+
+        <div style={{ marginTop: 20 }}> {JSON.stringify(inputList)} </div>
     </div>
+    
   );
 }
 
